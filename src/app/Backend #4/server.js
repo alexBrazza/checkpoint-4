e476e581090b440d23fd9725aeb17febabb0d8d9
Alexandre Cochet <alexcochet1@hotmail.fr>
2019-07-19 14:04:19 +0200

    improve version

--- src/app/Backend #4/server.js
+++ src/app/Backend #4/server.js
@@ -21,10 +21,56 @@ app.get('/spectacle', (req, res) => {
     }
     else
       res.json(results);
-      console.log("ça marche ")
+      console.log("Recup info spectacle ")
   });
 });
 
+app.get('/artists', (req, res) => {
+  connection.query('SELECT * FROM artists WHERE Id > 6', (err, results) => {
+    if (err) {
+      res.status(500).send('error');
+    }
+    else
+      res.json(results);
+      console.log("Recup Artist ")
+  });
+});
+
+app.delete('/show/:id', (req, res) => {
+
+  // récupération des données envoyées
+  const idShow = req.params.id;
+
+  // connexion à la base de données, et suppression de l'employé
+  connection.query('DELETE FROM spectacle WHERE id = ?', [idShow], err => {
+
+    if (err) {
+      // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
+      console.log(err);
+      res.status(500).send("Erreur lors de la suppression d'un employé");
+    } else {
+
+      // Si tout s'est bien passé, on envoie un statut "ok".
+      res.sendStatus(200);
+    }
+  });
+
+});
+
+app.post('/show', (req, response) => {
+
+  const formData = req.body;
+
+  connection.query('INSERT INTO spectacle SET ?', formData, (err,res ) => {
+
+    if (err) {
+      console.log(err);
+      response.status(500).send("Erreur lors de la sauvegarde d'une chanson");
+    } else {
+      response.sendStatus(200);
+    }
+  });
+});
 
 app.listen(port, (err) => {
   if (err) {
